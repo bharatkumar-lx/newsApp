593338007ce428e0899094c1e3c57f22ca0d0e59
bharatkumar-lx <bharatkumar987654321.bbk@gmail.com>
2021-10-22 12:10:25 +0530

    api is working

--- app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
@@ -2,14 +2,19 @@ package com.example.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsapp.R
+import com.example.newsapp.apis.RetrofitInstance
 import com.example.newsapp.databinding.ActivityNewsBinding
+import com.example.newsapp.utils.Constants.Companion.API_KEY
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-//api key c42d442de29c4c23a840a0af942e378a
 
 class NewsActivity : AppCompatActivity() {
 
@@ -22,9 +27,11 @@ class NewsActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
         setupActionBarWithNavController(navController)
-
-
-
+        lifecycleScope.launch(Dispatchers.IO){
+            Log.d("MainActivity",
+                RetrofitInstance.api.getBreakingNews().toString()
+            )
+        }
 
 
     }
---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object{
+        const val API_KEY = "c42d442de29c4c23a840a0af942e378a"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
